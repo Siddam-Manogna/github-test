5d616861d243968b8ccf91b27711705596499167
Siddam-Manogna <68559594+Siddam-Manogna@users.noreply.github.com>
2020-08-17 19:35:26 +0530

    Add files via upload

---
+++ Week4.java
@@ -0,0 +1,19 @@
+package com.dxc.logical;
+
+public class Week4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StudentDemo studentDemo=new StudentDemo("Manu", 1);
+				studentDemo.disp();
+				studentDemo.setName("Indhu");
+				//studentDemo.setRno(60);
+				studentDemo.disp();
+				studentDemo.getName();
+				studentDemo.disp();
+				
+		
+
+	}
+
+}
