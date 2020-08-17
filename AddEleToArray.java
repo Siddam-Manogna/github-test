dc81eaaadd4bb584915124e8329dfc554e6d5b0c
Siddam-Manogna <68559594+Siddam-Manogna@users.noreply.github.com>
2020-08-17 19:44:14 +0530

    Add files via upload

---
+++ AddEleToArray.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class AddEleToArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a = {1,2,3,4,5};
+	   
+	   int Index= 2;
+	   int newValue    = 5;
+	  System.out.println(Arrays.toString(a));     
+	   
+	   a[Index] = newValue;
+	   System.out.println(Arrays.toString(a));
+	 }
+
+	}
+
