a4d7fb55c0c556616a4e5e2f0e1c0505cf83e4fc
Siddam-Manogna <68559594+Siddam-Manogna@users.noreply.github.com>
2020-09-08 20:11:34 +0530

    Add files via upload

--- MyRestApi.java
+++ MyRestApi.java
@@ -1,5 +1,10 @@
-package com.myspr.springbootapi;
-
+package com.myapp.bootdemo;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,21 +13,49 @@ public class MyRestApi {
 
 
 @RequestMapping(value="test")
-public String hello()
-{
+public String hello(){
 
 return "Hello World";
 }
 
-
-
-@RequestMapping(value="student")
-public Student getStudent()
-{
-
+@GetMapping(value="student")
+public Student getStudent() {
 Student student=new Student(10,"DHONI");
 return student;
 }
+
+@GetMapping(value="student/{name}")
+public Student getStudentName(@PathVariable String name){
+Student student=new Student(10,name);
+return student;
+}
+
+
+@PostMapping(value="studentdata")
+public String postStudent(@RequestBody Student student)
+{
+System.out.println(student);
+return "Data accepted";
+}
+
+
+@DeleteMapping(value="studentdata")
+public String deleteStudent(@RequestBody String name)
+{
+System.out.println(name);
+return "delete requested";
+}
+
+
+@PutMapping(value="studentdata")
+public String putStudent()
+
+{
+return "putrequested";
+}
+
+
+
 
 
 }
--- Student.java
+++ Student.java
@@ -1,48 +1,31 @@
-package com.myapp.spring.bean;
+package com.myapp.bootdemo;
 
 public class Student {
-	String name;
-	String studentId;
-	String address;
-	int sem;
-	public String getName() {
-	return name;
-	}
-	public void setName(String name) {
-	this.name = name;
-	}
-	public String getStudentId() {
+	int studentId;
+	String studentName;
+	public int getStudentId() {
 	return studentId;
 	}
-	public void setStudentId(String studentId) {
+	public void setStudentId(int studentId) {
 	this.studentId = studentId;
 	}
-	public String getAddress() {
-	return address;
+	public String getStudentName() {
+	return studentName;
 	}
-	public void setAddress(String address) {
-	this.address = address;
+	public void setStudentName(String studentName) {
+	this.studentName = studentName;
 	}
-	public int getSem() {
-	return sem;
-	}
-	public void setSem(int sem) {
-	this.sem = sem;
-	}
-	public Student(String name, String studentId, String address, int sem) {
+	public Student(int studentId, String studentName) {
 	super();
-	this.name = name;
 	this.studentId = studentId;
-	this.address = address;
-	this.sem = sem;
-	}
-
-	public Student() {
-	// TODO Auto-generated constructor stub
+	this.studentName = studentName;
 	}
 	@Override
 	public String toString() {
-	return "Student [name=" + name + ", studentId=" + studentId + ", address=" + address + ", sem=" + sem + "]";
+	return "Student [studentId=" + studentId + ", studentName=" + studentName + "]";
+	}
+	public Student() {
+	// TODO Auto-generated constructor stub
+	}
 	}
 
-}
